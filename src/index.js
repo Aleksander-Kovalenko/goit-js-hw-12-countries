4b7cb7a5ab0b4c52a39545d9f8edf38781f376ef
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-10-30 19:19:56 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,42 +1,64 @@
 const Handlebars = require('handlebars');
 var debounce = require('lodash.debounce');
+
+const { alert, notice, info, success, error } = require('@pnotify/core');
+
 import './sass/main';
 import card from './template/cards.hbs';
 import itemList from './template/item-list.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
 
-const BASE_API = 'https://restcountries.com/v2/name/';
+const BASE_API = 'https://restcountries.com/v2/name';
 
 const refs = {
-  submitForm: document.querySelector('.js-button'),
   inputForm: document.querySelector('.js-input'),
 };
-refs.submitForm.addEventListener('click', e => {
-  e.preventDefault();
-});
 
 refs.inputForm.addEventListener('input', debounce(fetchCountry, 500));
 
 function fetchCountry(e) {
   e.preventDefault();
   const value = e.target.value;
-  console.log(value);
-  return fetch(`${BASE_API}${value}`)
+
+  return fetch(`${BASE_API}/${value}`)
     .then(response => response.json())
-    .then(listCountry)
-    .catch(console.log);
+    .then(listCountry);
 }
 
 function paintCard(country) {
-  const num = country.length;
-  console.log(num);
   const template = card({ country });
-
   document.querySelector('.js-box-card').insertAdjacentHTML('beforeend', template);
 }
 
 function listCountry(list) {
-  document.querySelector('.js-box-card').innerHTML = '';
-  document.querySelector('.js-country').innerHTML = '';
-  if (list.length === 1) return paintCard(list);
+  clear();
+  if (list.status) onError();
+  else if (list.length > 10) onInfo();
+  else if (list.length === 1) paintCard(list);
+  else if (list.length) creatList(list);
+}
+
+function clear() {
+  document.querySelector('.js-box-card').innerHTML = ' ';
+  document.querySelector('.js-country').innerHTML = ' ';
+}
+
+function onError() {
+  return error({
+    text: 'Sorry',
+    delay: 1500,
+  });
+}
+
+function onInfo() {
+  return alert({
+    text: "I'm an alert.",
+    type: 'info',
+    delay: 1500,
+  });
+}
+
+function creatList(list) {
   document.querySelector('.js-country').insertAdjacentHTML('beforeend', itemList({ list }));
 }
