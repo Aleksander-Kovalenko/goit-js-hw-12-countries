98d203c01072dec2bfaef0dc478ecadf45a02a78
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-10-30 18:09:53 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 const Handlebars = require('handlebars');
+var debounce = require('lodash.debounce');
 import './sass/main';
 import card from './template/cards.hbs';
+import itemList from './template/item-list.hbs';
 
 const BASE_API = 'https://restcountries.com/v2/name/';
 
-function fetchCountry(name) {
-  return fetch(`${BASE_API}${name}`)
+const refs = {
+  submitForm: document.querySelector('.js-button'),
+  inputForm: document.querySelector('.js-input'),
+};
+refs.submitForm.addEventListener('click', e => {
+  e.preventDefault();
+});
+
+refs.inputForm.addEventListener('input', debounce(fetchCountry, 500));
+
+function fetchCountry(e) {
+  e.preventDefault();
+  const value = e.target.value;
+  console.log(value);
+  return fetch(`${BASE_API}${value}`)
     .then(response => response.json())
-    .then(paintCard);
+    .then(listCountry)
+    .catch(console.log);
 }
 
 function paintCard(country) {
+  const num = country.length;
+  console.log(num);
   const template = card({ country });
 
-  document.body.insertAdjacentHTML('beforeend', template);
+  document.querySelector('.js-box-card').insertAdjacentHTML('beforeend', template);
 }
 
-fetchCountry('Switzerland');
+function listCountry(list) {
+  document.querySelector('.js-box-card').innerHTML = '';
+  document.querySelector('.js-country').innerHTML = '';
+  if (list.length === 1) return paintCard(list);
+  document.querySelector('.js-country').insertAdjacentHTML('beforeend', itemList({ list }));
+}
